9681bbc0dae9495466277707b9394a82b92d0d8f
josephmeadowcroft <149323761+josephmeadowcroft@users.noreply.github.com>
2024-04-12 09:41:01 +0100

    Created json and images

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./quiz.scss";
+import asEconomicsQuestions from "../data/asEconomicsQuestions.json";
+
+const Quiz = () => {
+  return (
+    <div className="quiz">
+      <h1>AQA AS Economics MCQs</h1>
+      <div className="questionArea">
+        <img src="../data" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Quiz;
