076bdd7e1971d38a28459c5643fa559bc194d1c5
josephmeadowcroft <149323761+josephmeadowcroft@users.noreply.github.com>
2024-04-12 13:31:43 +0100

    Disabled buttons after answer + next button appears

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -10,42 +10,27 @@ const Quiz = () => {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [correct, setCorrect] = useState(0);
   const [incorrect, setIncorrect] = useState(0);
+  const [answerSelected, setAnswerSelected] = useState(false);
 
   const currentQuestion = asEconomicsQuestions[currentQuestionIndex];
+
+  const handleQuestionAnswered = ({ target }) => {
+    setAnswerSelected(true);
+    // When correct answer selected:
+    if (target.value === currentQuestion.correctOption) {
+      setCorrect(correct + 1);
+    } else {
+      // When incorrect answer selected:
+      setIncorrect(incorrect + 1);
+    }
+  };
 
   const handleNextQuestion = () => {
     if (currentQuestionIndex + 1 < asEconomicsQuestions.length) {
       setCurrentQuestionIndex(currentQuestionIndex + 1);
+      setAnswerSelected(false);
     } else {
-      // Quiz ended, handle accordingly
-    }
-  };
-
-  const handlePreviousQuestion = () => {
-    if (currentQuestionIndex - 1 >= 0) {
-      setCurrentQuestionIndex(currentQuestionIndex - 1);
-    } else {
-      // Cannot go back
-    }
-  };
-
-  const handleQuestionAnswered = ({ target }) => {
-    // When correct answer selected:
-    if (target.value === currentQuestion.correctOption) {
-      setCorrect(correct + 1);
-      if (currentQuestionIndex + 1 < asEconomicsQuestions.length) {
-        setCurrentQuestionIndex(currentQuestionIndex + 1);
-      } else {
-        navigate("/results");
-      }
-    } else {
-      // When incorrect answer selected:
-      setIncorrect(incorrect + 1);
-      if (currentQuestionIndex + 1 < asEconomicsQuestions.length) {
-        setCurrentQuestionIndex(currentQuestionIndex + 1);
-      } else {
-        navigate("/results");
-      }
+      navigate("/results");
     }
   };
 
@@ -53,7 +38,7 @@ const Quiz = () => {
     <div className="quiz">
       <div className="header">
         <h1>AQA AS Economics MCQs</h1>
-        <p>Score {correct}</p>
+        <p>Score: {correct}</p>
         <p>Incorrect: {incorrect}</p>
       </div>
       <div className="questionArea">
@@ -65,20 +50,26 @@ const Quiz = () => {
         <div className="answerBtns">
           {currentQuestion.options.map((option, optionIndex) => (
             <label key={optionIndex}>
-              <button
+              <motion.button
                 name={`question${currentQuestion.question}`}
                 onClick={handleQuestionAnswered}
                 value={option}
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+                disabled={answerSelected}
               >
                 {option}
-              </button>
+              </motion.button>
             </label>
           ))}
         </div>
-        <div className="changeQuestionBtns">
-          <button onClick={handlePreviousQuestion}>Previous Question</button>
-          <button onClick={handleNextQuestion}>Next Question</button>
-        </div>
+        <button
+          id="nextQuestionBtn"
+          hidden={!answerSelected}
+          onClick={handleNextQuestion}
+        >
+          Next
+        </button>
       </div>
     </div>
   );
