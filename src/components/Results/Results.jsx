19c00aa172edabe8cb2fd52eff41903b0d0aafdd
josephmeadowcroft <149323761+josephmeadowcroft@users.noreply.github.com>
2024-04-12 12:49:01 +0100

    try again button

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import './results.scss'
+import React from "react";
+import "./results.scss";
+import { useNavigate } from "react-router-dom";
 
 const Results = () => {
-  return (
-    <div>Results</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default Results
+  return (
+    <div className="results">
+      <div className="allResults">
+        <h2>Results:</h2>
+        <button onClick={() => navigate("/quiz")}>Try again</button>
+      </div>
+    </div>
+  );
+};
+
+export default Results;
