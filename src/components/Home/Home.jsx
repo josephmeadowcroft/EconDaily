f38be3e70005d19d930c98a8622b316a719ba746
josephmeadowcroft <149323761+josephmeadowcroft@users.noreply.github.com>
2024-04-13 10:37:28 +0100

    Installed MUI

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import './home.scss'
+import React from "react";
+import "./home.scss";
 
 const Home = () => {
   return (
-    <div className='home'>Home</div>
-  )
-}
+    <div className="home">
+      <div className="header">
+        <h1>Exam Question Generator</h1>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
